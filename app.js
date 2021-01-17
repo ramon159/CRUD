dbb58683bd30564a623b2b95dc15d929f462581a
Ramon Arthur <ramonarthur14@gmail.com>
2021-01-16 21:21:13 -0300

    insert pronto

--- app.js
+++ app.js
@@ -2,10 +2,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
 const handlebars = require('express-handlebars');
-
+const { isNumber } = require('util');
+const urlEncodeParser = bodyParser.urlencoded({ extended: false });
 const app = express();
 const port = 3000;
-
+const sql = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    port: 3306,
+    database: 'crud',
+});
+sql.query('use crud');
 //
 const log = (...arg) => console.log(...arg);
 
@@ -24,14 +32,35 @@ app.get('/style', (req, res) => {
     res.sendFile(__dirname + '/css/style.css');
 });
 
-app.get('/insert', (req, res) => {res.render('insert')});
-app.get('/delete', (req, res) => {res.render('delete')});
-app.get('/update', (req, res) => {res.render('update')});
-app.get('/read', (req, res) => {res.render('read')});
-app.post('/controllerForm', (req, res) => {res.render('controllerForm')})
+app.get('/insert', (req, res) => {
+    res.render('insert');
+});
+app.get('/delete', (req, res) => {
+    res.render('delete');
+});
+app.get('/update', (req, res) => {
+    res.render('update');
+});
+app.get('/read', (req, res) => {
+    res.render('read');
+});
 app.get('/', (req, res) => {
-    res.render('index', { id: req.params.id });
-    /*     console.log(req.params.id) */
+
+    res.render('index', {id: 30});
+});
+app.post('/controllerForm', urlEncodeParser, (req, res) => {
+    const r = req.body;
+    const name = r.name;
+    const age = r.age;
+    if (!isNaN(age) && name) {
+        sql.query('insert into user (name, age) values (?,?)', [name, age]);
+        console.log(`dados inseridos: ${name}, ${age}`)
+        res.render('insert', {msg: " Dados inseridos com sucesso", code: 1})
+    } else {
+        res.render('insert', {msg: "Falha ao inserir dados", code: 0})
+    }
+
+
 });
 
 //start server
