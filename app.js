018165265aea21b8c0d264dd629dd4d762563054
Ramon Arthur <ramonarthur14@gmail.com>
2021-01-25 23:56:03 -0300

    update e delete finalizados

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const mysql = require('mysql');
 const handlebars = require('express-handlebars');
 const { isNumber } = require('util');
-const urlEncodeParser = bodyParser.urlencoded({ extended: false });
+
 const app = express();
+app.use(express.urlencoded({ extended: false }))
+
 const port = 3000;
 const sql = mysql.createConnection({
     host: 'localhost',
@@ -35,26 +36,37 @@ app.get('/style', (req, res) => {
 app.get('/insert', (req, res) => {
     res.render('insert');
 });
-app.get('/delete', (req, res) => {
-    res.render('delete');
-});
-app.get('/update', (req, res) => {
-    res.render('update');
-});
 app.get('/read', (req, res) => {
     sql.query("SELECT * FROM user", (error, result, fields) => {
         res.render('read', {data: result});
     })
 });
+app.post('/update', (req, res) => {
+    const r = req.body;
+    const id = r.id;
+    const name = r.name;
+    const age = r.age;
+    if (Number.isInteger(parseInt(id)) && Number.isInteger(parseInt(age)) && name) {
+        sql.query("UPDATE user set name = ?, age = ? where id = ?", [name, age, id])
+        console.log(`Dados atualizados: [${id}] `, name, age)
+        res.json({id: id, name: name, age: age})
+    }
+});
+app.post('/delete', (req, res) => {
+    const id = req.body.id;
+    sql.query("DELETE FROM user WHERE id = ?", [id])
+    log('dados deletados do ID: ', id)
+    res.json({id: id})
+});
 app.get('/', (req, res) => {
 
-    res.render('index', {id: 30});
+    res.redirect('/read')
 });
-app.post('/controllerForm', urlEncodeParser, (req, res) => {
+app.post('/controllerForm', (req, res) => {
     const r = req.body;
     const name = r.name;
     const age = r.age;
-    if (!isNaN(age) && name && age > 0) {
+    if (Number.isInteger(parseInt(age))  && name && age > 0) {
         sql.query('insert into user (name, age) values (?,?)', [name, age]);
         console.log(`dados inseridos: ${name}, ${age}`)
         res.render('insert', {msg: " Dados inseridos com sucesso", code: 1})
@@ -66,6 +78,6 @@ app.post('/controllerForm', urlEncodeParser, (req, res) => {
 
 //start server
 app.listen(port, (req, res) => {
-    log('funfando');
+    log('[*] Server running...');
 });
 app.get('/', (req, res) => {});
--- js/javascript.js
+++ js/javascript.js
@@ -1,18 +1,49 @@
-let test = 0;
+const sendData = function(url, method, data) {
+    $.ajax({
+    url: url,
+    method: method,
+    data: data,
+    error: (err) => {console.log(err)}
+
+})
+console.log(url, method, data)
+}
+
+
 const editData = (self) => {
     const tr = self.parentNode.parentNode;
     const name = tr.getElementsByClassName('name')[0]
+    const innerName = tr.getElementsByClassName('innerName')[0]
     const age = tr.getElementsByClassName('age')[0]
+    const innerAge = tr.getElementsByClassName('innerAge')[0]
+
 
    if (name.style.display == 'none'){
-        name.style.display = 'block';
-        age.style.display = 'block';
+        name.style.display = 'inline';
+        age.style.display = 'inline';
+
+        innerName.style.display = 'none';
+        innerAge.style.display = 'none';
     } else {
+        sendData('/update', 'post', {id: tr.id, name: name.value, age: age.value})
+        innerAge.innerText = age.value;
+        innerName.innerText = name.value
+        console.log(name.value, age.value)
+
         name.style.display = 'none';
         age.style.display = 'none';
+
+        innerName.style.display = '';
+        innerAge.style.display = '';
     }
 };
 const deleteData = (self) => {
-    trId = self.parentNode.parentNode.id
-    console.log(trId)
+    tr = self.parentNode.parentNode;
+    trId = tr.id;
+    table = tr.parentNode;
+    
+    console.log(trId, tr, table)
+    sendData('/delete', 'post', {id: trId})
+    
+    table.removeChild(tr);
 };
